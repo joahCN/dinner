bbf5d79e4ea7cff101f7e4d99338f49b0f1a9ac8
joah <448363903@qq.com>
2017-03-15 17:05:50 +0800

    add receive wx text message interface

--- api/controllers/WxController.js
+++ api/controllers/WxController.js
@@ -7,10 +7,26 @@
 
 // let respondWrap = require('../services/respondWrap');
 
+var builder = require('xmlbuilder');
+
 module.exports = {
     index: (req, res)=>{
         let {echostr} = req.query;
         res.send(echostr);
+    },
+    xml: (req, res)=>{
+        let {xml: {ToUserName, FromUserName, MsgType, Content}} = req.body;
+        console.log(req.body);
+        var root = builder.create('xml', {headless: true});
+            root.ele('ToUserName').dat(FromUserName);
+            root.ele('FromUserName').dat(ToUserName);
+        root.ele('CreateTime').dat((new Date()).getTime());
+        root.ele('MsgType').dat("text");
+        root.ele('Content').dat("Hello, " + ToUserName);
+
+        var xml = root.end({ pretty: true});
+        res.set('Content-Type', 'application/xml');
+        res.send(xml);
     }
 };
 
--- config/http.js
+++ config/http.js
@@ -9,6 +9,7 @@
  * http://sailsjs.org/#!/documentation/reference/sails.config/sails.config.http.html
  */
 
+
 module.exports.http = {
 
   /****************************************************************************
@@ -76,6 +77,22 @@ module.exports.http = {
   ***************************************************************************/
 
     // bodyParser: require('skipper')({strict: true})
+
+  bodyParser: (function() {
+      // Initialize a skipper instance with the default options
+      var skipper = require('skipper')();
+      // Initialize an express-xml-bodyparser instance with the default options
+      var xmlparser = require('express-xml-bodyparser')({trim: true, explicitArray: false, normalizeTags: false});
+      // Create and return the middleware function
+      return function(req, res, next) {
+          // If we see an application/xml header, parse the body as XML
+          if (req.headers['content-type'] == 'application/xml') {
+              return xmlparser(req, res, next);
+          }
+          // Otherwise use Skipper to parse the body
+          return skipper(req, res, next);
+      };
+  })()
 
   },
 
